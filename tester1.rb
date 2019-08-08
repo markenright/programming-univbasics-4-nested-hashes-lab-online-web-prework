320f22ff2ee74b9ee6e13c042bb775cb3e1d90a3
Mark Enright <enright.mark@gmail.com>
2019-08-08 17:36:04 +0000

    Done.

---
+++ tester1.rb
@@ -0,0 +1,17 @@
+programmer_hash = 
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+     
+    puts programmer_hash[:dennis_ritchie][:languages]
